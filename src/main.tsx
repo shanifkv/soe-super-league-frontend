784af8f941501fe33643060458e27bad7467a0d2
ajuroshan <ajuaju0483@gmail.com>
2025-12-31 05:31:22 +0530

    chore: Set application basename to '/' unconditionally.

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-const basename = import.meta.env.PROD ? "/app" : "/";
+const basename = "/";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
